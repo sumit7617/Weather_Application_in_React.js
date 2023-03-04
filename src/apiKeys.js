2a379cc35020d052d113540efecdc02dce1bb6a7
sumit7617 <sumitgaikwad221515@gmail.com>
2023-03-05 00:24:46 +0530

    final

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ba3a310bdbdcf996a4d13b3eaa4e6992",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
